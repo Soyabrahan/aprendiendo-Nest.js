6da4993e272b79333cc39cc4e11aae1be3b85da8
Abrahan <abrahanramirez2410@gmail.com>
2025-05-11 16:42:12 -0400

    servicios

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,9 +1,20 @@
 import { Controller, Get } from "@nestjs/common";
-
+import { TasksService } from "./task.service";
+//en los controladores se aplica la logica es basicamente una funcion que se va a generar dependiendo la ruta que se llame
 @Controller({}) // Path base
 export class TasksController {
-    @Get("/tasks") // Ruta relativa: GET /tasks
-    getAllTasks() {
-        return 'obteniendo todas las tareas';
+
+    tasksService : TasksService
+
+    constructor(tasksService:TasksService){
+        this.tasksService = tasksService;
     }
+
+    @Get("/tasks") // Ruta relativa: GET /tasks
+
+    getAllTasks() {
+        // puedo buscar en una bd o hacer una peticion
+        return this.tasksService.getTasks();
+    }
+
 }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,10 @@
 import {Module} from "@nestjs/common";
 import { TasksController } from "./task.controller";
+import { TasksService } from "./task.service";
 
 //app.module contiene muchos modulos y un modulo contiene controladores
 @Module({
-    controllers:[TasksController]
+    controllers:[TasksController],
+    providers : [TasksService],
 })
 export class TasksModule {}
