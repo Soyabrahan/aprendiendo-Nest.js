26ca0cba1e2768a2412b6efc1562ffa4d54c7818
Abrahan <abrahanramirez2410@gmail.com>
2025-05-19 23:47:13 -0400

    pagina inicio

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+import { HelloController } from './hello/hello.controller';
 
 @Module({
   imports: [TasksModule, UsersModule],
-  controllers: [AppController],
+  controllers: [AppController, HelloController],
 })
 export class AppModule {}
